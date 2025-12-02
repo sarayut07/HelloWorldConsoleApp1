98fb4c571229d65768fff6f875324b5a5a6ff27f
sarayut07 <sarayut.saa@kkumail.com>
2025-12-02 10:46:17 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("My nema is Sarayut Saut");
+            Console.WriteLine("Student ID: 683450062-4");
+            Console.WriteLine("Myjor: Computer and Information Science");
+            Console.WriteLine("Faculty: ISKKu");
+            Console.WriteLine("Khon Kaen University");
+
+        }
+    }
+}
